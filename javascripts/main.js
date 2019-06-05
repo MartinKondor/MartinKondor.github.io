10e23b3b36887795bc15c066277e0a730902a08e
Martin Kondor <martinkondor@gmail.com>
2019-06-05 22:10:14 +0200

    Remove dependency, changing style, adding certification

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,30 +1,10 @@
 (function ($) {
-
     var inDarkMode = false;
-
+    var increasedImg = [];
 
     document.getElementById('copyright').firstChild.data += `
         ${new Date().getFullYear()}
     `;
-
-    /*
-    * Animations
-    */
-    new Bounce().translate({
-        from: { x: -500, y: -50 },
-        to: { x: 0, y: 0 },
-        duration: 3500,
-        stiffness: 10
-    })
-    .applyTo(document.querySelectorAll('.translate-from-left-animation'));
-
-    new Bounce().translate({
-        from: { x: 500, y: 50 },
-        to: { x: 0, y: 0 },
-        duration: 3500,
-        stiffness: 10
-    })
-    .applyTo(document.querySelectorAll('.translate-from-right-animation'));
 
     /*
     * Dark mode
