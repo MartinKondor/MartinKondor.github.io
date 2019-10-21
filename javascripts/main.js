9e4567b53e009e31ce3fad5d3bfcec319ac6178f
Martin Kondor <martinkondor@gmail.com>
2019-10-21 21:47:36 +0200

    Update for work images

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,23 +1,5 @@
-(function ($) {
-    var inDarkMode = false;
-    var increasedImg = [];
-
+(function () {
     document.getElementById('copyright').firstChild.data += `
         ${new Date().getFullYear()}
     `;
-
-    /*
-    * Dark mode
-    */
-    $('#main-title').on('click', function (e) {
-        if (!inDarkMode) {
-            $('body').attr('style', 'filter: invert(100%);');
-            inDarkMode = true;
-        }
-        else {
-            $('body').attr('style', 'filter: invert(0%);');
-            inDarkMode = false;
-        }
-    });
-
-})(jQuery);
+})();
