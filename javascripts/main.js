589b2c972436e377c4d884d877fb3081d6e81bda
Martin Kondor <martinkondor@gmail.com>
2020-07-09 10:24:50 +0200

    Face change

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,3 +1,14 @@
-(function () {
-    document.getElementById('copyright').innerHTML = document.getElementById('copyright').innerHTML + new String(new Date().getFullYear());
-})();
+(($) => {
+
+    // Dark mode after 6pm and before 7am
+    let t = new Date().getHours();
+    if (t < 7 || t > 18) {
+        $('body').css('background-color', 'rgb(0, 0, 34)');
+        $('body').css('color', '#fefefe');
+        $('#logo').attr('src', 'img/logo.jpg');
+    }
+
+    // Change copyright year
+    $('#copyright').html($('#copyright').html() + new String(new Date().getFullYear()));
+
+})(jQuery);
