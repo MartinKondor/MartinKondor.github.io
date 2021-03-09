9c786a0018a943eeb64db8b8174ac999509b7c76
Kondor Martin <martinkondor@mm.local>
2021-03-09 13:01:37 +0100

    Adding floating elements

--- js/index.js
+++ js/index.js
@@ -18,12 +18,26 @@
     function darkMode() {
         $('body').css('color', 'rgb(248, 248, 248)');
         $('body').css('background-color', 'rgb(0, 0, 34)');
+        $('.bg-particles').css('background-color', 'rgb(0, 0, 34)');
+        
         $('#home').css('background-image', 'none');
         $('#about-me').css('background-image', 'none');
         $('h1').css('color', 'rgb(248, 248, 248)');
         $('h2').css('color', 'rgb(248, 248, 248)');
-        $('#skillset').css('background-color', 'rgb(248, 248, 248, 0.7)');
-        $('#skillset').css('padding', '10px');
+
+        $('#skillset > i').css('background-color', 'rgb(200, 200, 234, 0.7)');
+        $('#skillset > img').css('background-color', 'rgb(200, 200, 234, 0.7)');
+        $('#skillset > span').css('background-color', 'rgb(200, 200, 234, 0.7)');
+        $('#skillset > i').css('padding', '30px');
+        $('#skillset > img').css('padding', '30px');
+        $('#skillset > span').css('padding', '30px');
+        $('#skillset > i').css('border-radius', '100%');
+        $('#skillset > img').css('border-radius', '100%');
+        $('#skillset > span').css('border-radius', '100%');
+
+        $('#skillset > span').css('color', 'rgb(0, 0, 34)');
+
+        $('.bg-particles li').css('background-color', 'rgb(20, 20, 54)');
     }
 
     $('#dark-mode-btn').on('click', function () {
