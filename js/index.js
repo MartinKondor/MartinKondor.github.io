318f11649c910c9f8d7bb58578b44d2cdf31751c
Martin Kondor <martinkondor@gmail.com>
2021-07-27 20:15:20 +0200

    Adding visitor counter, remove bg

--- js/index.js
+++ js/index.js
@@ -4,6 +4,11 @@
     // Update copyright
     $('#copyright').html($('#copyright').html() + new String(new Date().getFullYear()));
     
+    $('#vis-counter-btn').on('click', function () {
+        $('#vis-counter').css('display', 'block');
+    });
+
+    /*
     var w = $(document).width();
     var h = $(document).height() + 1;
     var w_i = 0;
@@ -33,5 +38,6 @@
             }
         }
     }, 2);
+    */
 
 })(jQuery);
