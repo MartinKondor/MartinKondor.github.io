eebe7df46fc0651956bfd0f36c5266e3c86563d4
Martin Kondor <martinkondor@gmail.com>
2021-08-23 19:08:25 +0200

    Remove BG for better SEO

--- js/index.js
+++ js/index.js
@@ -8,34 +8,4 @@
         $('#vis-counter').css('display', 'block');
     });
 
-    var w = $(document).width();
-    var h = $(document).height() + 1;
-    var w_i = 0;
-    var h_i = 0;
-    var s = '';
-
-    var bgInterval = setInterval(function ()
-    {
-        s = new String(Math.random() >= 0.3 ? 1 : 0);
-
-        if (Math.random() <= 0.3)
-        {
-            s += '\n';
-        }
-
-        if (Math.random() <= 0.017)
-        {
-            s += '<br>';
-            ++h_i;
-        }
-
-        $('#bg').html($('#bg').html() + s);
-
-        if (++w_i >= w) {
-            if (++h_i >= h) {
-                clearInterval(bgInterval);
-            }
-        }
-    }, 2);
-
 })(jQuery);
