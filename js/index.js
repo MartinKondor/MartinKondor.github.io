a154c558d0b4d0874b78447b24b4e9ce038e1553
Martin Kondor <martinkondor@gmail.com>
2022-03-03 11:55:08 +0100

    Adding essays

--- js/index.js
+++ js/index.js
@@ -1,5 +1,24 @@
-(function ($)
-{
+(function ($) {
+
+    const LANG = {
+        "hu-HU": {
+            "#name": "Kondor Martin",
+            "#introduction": "Szoftverfejlesztő. A Pannon Egyetem Mérnökinformatikus BSc szakos hallgatója.",
+            "#software-title": "Szoftverek",
+            "#apps-title": "Alkalmazások",
+            "#essays-title": "Esszék",
+            //"#papers-title": "Cikkek",
+        }
+    };
+    let userLang = navigator.language || navigator.userLanguage;
+    userLang = null;
+
+    // Change html according to language
+    if (userLang === "hu-HU") {
+        for (let k of Object.keys(LANG["hu-HU"])) {
+            $(k).html(LANG["hu-HU"][k]);
+        }
+    }
 
     // Update copyright
     $('#copyright').html(new String(new Date().getFullYear()));
