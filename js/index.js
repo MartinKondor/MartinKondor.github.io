d15132764564c1823372fe26d9168f0b4bc101c5
Martin Kondor <martinkondor@gmail.com>
2022-05-29 17:50:42 +0200

    Huge update

--- js/index.js
+++ js/index.js
@@ -23,8 +23,5 @@
     // Update copyright
     $('#copyright').html(new String(new Date().getFullYear()));
 
-    $('#vis-counter-btn').on('click', function () {
-        $('#vis-counter').css('display', 'block');
-    });
 
 })(jQuery);
