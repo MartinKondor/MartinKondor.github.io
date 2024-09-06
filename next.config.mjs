9e0ed81f8f8df737c34c199496cf09244f438e65
Martin Kondor <martinkondor@gmail.com>
2024-09-06 20:30:14 +0200

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
+  reactStrictMode: true,
   basePath: process.env.NEXT_PUBLIC_BASE_PATH || "",
   assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || "",
-  output: "export",
 };
 
 export default nextConfig;
